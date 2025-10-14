d3e57f6511d6198a6442137e5c5b30c536897f7d
vputra138-star <vputra138@gmail.com>
2025-10-14 19:39:09 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="methodegetproses.php" method="get">
+        Masukkan nama:
+        <input type="text" name="nama" size="25">
+        <input type="submit" value="proses">
+
+</body>
+</html>
